bef48ec7052526531bdefc94ca31ae188b00fbf5
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-10-12 14:23:20 +0300

    Adds show method to the CouncilsController

--- app/controllers/councils_controller.rb
+++ app/controllers/councils_controller.rb
@@ -1,7 +1,21 @@
 class CouncilsController < ApplicationController
+
+    wrap_parameters false
    #GET index 
     def index 
         council_members = Council.all 
         render json: council_members, status: :ok
     end
+
+    #GET show 
+    def show 
+        council_member = find_council_member
+        render json: council_member, status: :ok
+    end
+
+    private 
+    def find_council_member 
+        Council.find(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :councils, only: [:index]
+  resources :councils, only: [:index,:show]
   resources :activities, only: [:index, :show,:create, :update, :destroy]
   resources :members, only: [:index, :destroy, :show]
 end
