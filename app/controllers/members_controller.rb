ae6986ebfab4001d1a02fad667088248bab39919
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-10-12 11:42:24 +0300

    Adds destroy action to the MembersController along with helper finde_member method

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,8 +1,27 @@
 class MembersController < ApplicationController
 
+ #GET index
     def index 
         members = Member.all 
-        render json: members
+        render json: members, status: :ok
     end
+
+#POST create 
+
+#PATCH update 
+
+#DELETE destroy 
+   def destroy 
+     member_to_delete = find_member;
+     member_to_delete.destroy
+     head :no_content
+   end
+
+   private 
+
+   def find_member 
+     Member.find(params[:id])
+   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :members, only: [:index]
+  resources :members, only: [:index, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
