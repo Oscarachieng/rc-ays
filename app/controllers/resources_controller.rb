94585e32bf1ecc336143659451cb1a523d0cfb0d
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-10-12 16:12:46 +0300

    Adds index action to ResourcesController

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,2 +1,9 @@
 class ResourcesController < ApplicationController
+    wrap_parameters false
+
+    #GET index 
+    def index 
+        resources = Resource.all 
+        render json: resources, status: :ok
+    end
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,4 @@
 class Resource < ApplicationRecord
     validates :title,:description,:image_url,:theme, presence: true 
-    
-    
+      
 end
